3ef8e2c7e1825e20924c85e54a3fdc954beaaddc
杨成锴 <243852814@qq.com>
2022-04-15 10:27:12 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OpenWrt-MIWiFi4a
 自用小米路由4a的OpenWrt自动化编译
+
+## 特别鸣谢
+
+1. [hugcabbage/shared-lede](https://github.com/hugcabbage/shared-lede)
+2. [KFERMercer/OpenWrt-CI](https://github.com/KFERMercer/OpenWrt-CI)
+3. [coolsnowwolf/lede](https://github.com/coolsnowwolf/lede)
---
+++ preset-models/2modify.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#修改登录IP
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+#修改主机名
+sed -i 's/OpenWrt/Xiaomi-Router/g' package/base-files/files/bin/config_generate
+#删除自带低版本xray-core
+rm -rf feeds/packages/net/xray-core
+rm -rf package/feeds/packages/xray-core
+#配置ipv6、主题
+sed -i '/exit 0/d' package/default-settings/files/zzz-default-settings
+cat default-settings/config_ipv6 >> package/default-settings/files/zzz-default-settings
+cat default-settings/config_theme >> package/default-settings/files/zzz-default-settings
+echo "exit 0" >> package/default-settings/files/zzz-default-settings
+#复制内核5.10版本CPU超频补丁
+#\cp -rf extra_files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+#设置WIFI
+#sed -i 's/OpenWrt/coolxiaomi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/wireless.default_radio${devidx}.encryption=none/wireless.default_radio${devidx}.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '/encryption/a\set wireless.default_radio${devidx}.key=coolxiaomi' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#修改登录密码为coolxiaomi
+#sed -i '/root/croot:$1$CBd7u73H$LvSDVXLBrzpk4JfuuN.Lv1:18676:0:99999:7:::' package/base-files/files/etc/shadow
+#切换ramips内核到5.15
+#sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.15' target/linux/ramips/Makefile
